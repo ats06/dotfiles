0aab32c4272d90165de9bec38e0afc144f161f1d
Atsushi Matsuura <Atsushi.Matsuura@sony.com>
2020-06-12 13:09:58 +0900

    modify init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,6 +8,9 @@
 ;; スタートアップメッセージを非表示
 (setq inhibit-startup-screen t)
 
+;; エラー音を鳴らなくする
+(setq ring-bell-function 'ignore)
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; 4.1 効率的な設定ファイルの作り方と管理方法             ;;
@@ -46,7 +49,9 @@
   ;; tool-barを非表示
   (tool-bar-mode 0)
   ;; scroll-barを非表示
-  (scroll-bar-mode 0))
+  (scroll-bar-mode 0)
+  ;; 行番号を表示
+  (global-linum-mode t))
 
 ;; CocoaEmacs以外はメニューバーを非表示
 (unless (eq window-system 'ns)
@@ -71,3 +76,268 @@
 (define-key global-map (kbd "C-c l") 'toggle-truncate-lines)
 ;; "C-t" でウィンドウを切り替える。初期値はtranspose-chars
 (define-key global-map (kbd "C-t") 'other-window)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.3 環境変数の設定                                     ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; P83 パスの設定
+;;(add-to-list 'exec-path "/opt/local/bin")
+;;(add-to-list 'exec-path "/usr/local/bin")
+
+;;; P85 文字コードを指定する
+(set-language-environment "Japanese")
+(prefer-coding-system 'utf-8)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.4 フレームに関する設定                               ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; P87-90 モードラインに関する設定
+;; カラム番号も表示
+(column-number-mode t)
+
+;; 行番号を表示
+(line-number-mode t)
+
+;; ファイルサイズを表示
+(size-indication-mode t)
+;; 時計を表示（好みに応じてフォーマットを変更可能）
+;; (setq display-time-day-and-date t) ; 曜日・月・日を表示
+(setq display-time-24hr-format t) ; 24時表示
+(display-time-mode t)
+;; バッテリー残量を表示
+(display-battery-mode t)
+
+;; リージョン内の行数と文字数をモードラインに表示する（範囲指定時のみ）
+;(defun count-lines-and-chars ()
+;  (if mark-active
+;      (format "(%dlines,%dchars) "
+;              (count-lines (region-beginning) (region-end))
+;              (- (region-end) (region-beginning)))
+;    ""))
+
+;(add-to-list 'default-mode-line-format
+;             '(:eval (count-lines-and-chars)))
+
+;; タイトルバーにファイルのフルパスを表示
+(setq frame-title-format "%f")
+
+;; 行番号を常に表示する
+;;(global-linum-mode t)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.5インデントの設定                                    ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; P91-92 タブ文字の表示幅
+;; TABの表示幅。初期値は8
+(setq-default tab-width 4)
+
+;; インデントにタブ文字を使用しない
+(setq-default indent-tabs-mode nil)
+
+;; C、C++、JAVA、PHPなどのインデント
+(add-hook 'c-mode-common-hook
+          '(lambda ()
+             (c-set-style "stroustrup")))
+
+;; C++ style
+(add-hook 'c++-mode-hook
+          '(lambda()
+             (c-set-style "stroustrup")
+             (setq indent-tabs-mode nil)     ; インデントは空白文字で行う（TABコードを空白に変換）
+             (c-set-offset 'innamespace 0)   ; namespace {}の中はインデントしない
+             (c-set-offset 'arglist-close 0) ; 関数の引数リストの閉じ括弧はインデントしない
+             ))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.6 表示・装飾に関する設定                             ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; P95 フェイス
+;; リージョンの背景色を変更
+;; (set-face-background 'region "darkgreen")
+
+;;; P96-97 フォントの設定
+;; AsciiフォントをMenloに
+;(set-face-attribute 'default nil
+;                    :family "Menlo"
+;                    :height 120)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.7 ハイライトの設定                                   ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; P98 現在行のハイライト
+;; (defface my-hl-line-face
+;;   ;; 背景がdarkならば背景色を紺に
+;;   '((((class color) (background dark))
+;;      (:background "NavyBlue" t))
+;;     ;; 背景がlightならば背景色を青に
+;;     (((class color) (background light))
+;;      (:background "LightSkyBlue" t))
+;;     (t (:bold t)))
+;;   "hl-line's my face")
+;; (setq hl-line-face 'my-hl-line-face)
+;; (global-hl-line-mode t)
+
+;; P99 括弧の対応関係のハイライト
+;; paren-mode：対応する括弧を強調して表示する
+(setq show-paren-delay 0) ; 表示までの秒数。初期値は0.125
+(show-paren-mode t) ; 有効化
+;; parenのスタイル: expressionは括弧内も強調表示
+(setq show-paren-style 'expression)
+;; フェイスを変更する
+;(set-face-background 'show-paren-match-face nil)
+;(set-face-underline-p 'show-paren-match-face "darkgreen")
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.8 バックアップとオートセーブ                         ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; P100-101 バックアップとオートセーブの設定
+;; バックアップとオートセーブファイルを~/.emacs.d/backups/へ集める
+(add-to-list 'backup-directory-alist
+             (cons "." "~/.emacs.d/backups/"))
+(setq auto-save-file-name-transforms
+      `((".*" ,(expand-file-name "~/.emacs.d/backups/") t)))
+
+;; オートセーブファイル作成までの秒間隔
+(setq auto-save-timeout 15)
+;; オートセーブファイル作成までのタイプ間隔
+(setq auto-save-interval 60)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.9 変更されたファイルの自動更新                       ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; P102 ファイルの自動更新
+;; 更新されたファイルを自動的に読み込み直す
+(global-auto-revert-mode t)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.10 フック                                            ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; P103 自動化のしくみ
+;; ファイルが #! から始まる場合、+xを付けて保存する
+(add-hook 'after-save-hook
+          'executable-make-buffer-file-executable-if-script-p)
+
+;;; P104-105 関数を定義する場合
+;; emacs-lisp-mode-hook用の関数を定義
+(defun elisp-mode-hooks ()
+  "lisp-mode-hooks"
+  (when (require 'eldoc nil t)
+    (setq eldoc-idle-delay 0.2)
+    (setq eldoc-echo-area-use-multiline-p t)
+    (turn-on-eldoc-mode)))
+
+;; emacs-lisp-modeのフックをセット
+(add-hook 'emacs-lisp-mode-hook 'elisp-mode-hooks)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 6.1 Elispをインストールしよう                          ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; P112-113 ELPAリポジトリを追加する
+(require 'package) ; package.elを有効化
+;; パッケージリポジトリにMarmaladeとMELPAを追加
+(add-to-list
+ 'package-archives
+ '("marmalade" . "https://marmalade-repo.org/packages/"))
+(add-to-list
+ 'package-archives
+ '("melpa" . "https://melpa.org/packages/"))
+(package-initialize) ; インストール済みのElispを読み込む
+;; 最新のpackageリストを読み込む
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 6.2 テーマ                                             ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; P119-121 テーマの変更
+(package-install 'zenburn-theme)
+;; zenburnテーマを利用する
+(load-theme 'zenburn t)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 6.3 統一したインタフェースでの操作                     ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; P122-123 候補選択型インタフェース Helm
+(package-install 'helm)
+
+;; Helm
+(require 'helm-config)
+
+
+;;; P125 過去の履歴からペーストする──helm-show-kill-ring
+;; M-yにhelm-show-kill-ringを割り当てる
+(define-key global-map (kbd "M-y") 'helm-show-kill-ring)
+
+;;; P126 moccurを利用する──helm-c-moccur
+(package-install 'helm-c-moccur)
+(when (require 'helm-c-moccur nil t)
+  (setq
+   ;; 執筆時点でエラーが出たため定義しているが、
+   ;; 将来的には不要になる可能性が高い
+   helm-idle-delay 0.1
+   ;; helm-c-moccur用 `helm-idle-delay'
+   helm-c-moccur-helm-idle-delay 0.1
+   ;; バッファの情報をハイライトする
+   helm-c-moccur-higligt-info-line-flag t
+   ;; 現在選択中の候補の位置をほかのwindowに表示する
+   helm-c-moccur-enable-auto-look-flag t
+   ;; 起動時にポイントの位置の単語を初期パターンにする
+   helm-c-moccur-enable-initial-pattern t)
+  ;; C-M-oにhelm-c-moccur-occur-by-moccurを割り当てる
+  (global-set-key (kbd "C-M-o") 'helm-c-moccur-occur-by-moccur))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Follows my original                  ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; gtags
+(package-install 'helm-gtags)
+(require 'helm-gtags)
+
+(add-hook 'c-mode-hook 'helm-gtags-mode)
+(add-hook 'c++-mode-hook 'helm-gtags-mode)
+(add-hook 'java-mode-hook 'helm-gtags-mode)
+
+;; key bindings
+(add-hook 'helm-gtags-mode-hook
+          '(lambda ()
+             (local-set-key (kbd "M-t") 'helm-gtags-find-tag)
+             (local-set-key (kbd "M-r") 'helm-gtags-find-rtag)
+             (local-set-key (kbd "M-s") 'helm-gtags-find-symbol)
+             (local-set-key (kbd "C-t") 'helm-gtags-pop-stack)))
+
+;; Scroll
+(setq scroll-conservatively 1)    ;; scroll 1 line
+(setq scroll-margin 5)            ;; start scroll before top or end of buffer
+
+;; backup open recent
+(when (require 'recentf nil t)
+  (setq recentf-max-saved-items 2000)
+  (setq recentf-exclude '(".recentf"))
+  (setq recentf-auto-cleanup 10)
+  (setq recentf-auto-save-timer
+        (run-with-idle-timer 30 t 'recentf-save-list))
+  (recentf-mode 1))
+
